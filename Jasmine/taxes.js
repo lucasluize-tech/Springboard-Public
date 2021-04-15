60e463bfa7b2b212e94900364cd6f395e2c364e4
lucasluize-tech <lucasluize.tech@gmail.com>
2021-04-15 12:09:28 -0400

    changes made to jasmine folder

--- Jasmine/taxes.js
+++ Jasmine/taxes.js
@@ -1,6 +1,6 @@
 function calculateTaxes(income){
  if (!Number.isFinite(income)){
-     throw new Error('BAD INCOME');
+     throw new Error('INVALID INCOME');
  }
  
  if (income>30000){
@@ -9,4 +9,23 @@ function calculateTaxes(income){
      return income * 0.15;
  }
  
+}
+
+function removeDuplicates(value){
+    const arr = [...new Set(value)]
+    if (typeof value === 'string') return arr.join('')
+    return arr;
+}
+
+function remove(arr, val){
+    return arr.filter((el)=>{
+        return el !== val
+    })
+}
+
+let usernames =[];
+let input = document.querySelector('.input');
+
+function submitUser(){
+    usernames.push(input.value)
 }
--- Jasmine/taxes.test.js
+++ Jasmine/taxes.test.js
@@ -1,16 +1,62 @@
-it('should calculate low-bracket', function(){
-    expect(calculateTaxes(10000)).toEqual(1500);
-    expect(calculateTaxes(20000)).toEqual(3000);
+describe('calculateTaxes tests', function(){
+    it('should calculate low-bracket', function(){
+        expect(calculateTaxes(10000)).toEqual(1500);
+        expect(calculateTaxes(20000)).toEqual(3000);
+    })
+
+    it('sould calculate high-bracket', function(){
+        expect(calculateTaxes(40000)).toEqual(10000);
+    })
 })
 
-it('sould calculate high-bracket', function(){
-    expect(calculateTaxes(40000)).toEqual(10000);
+describe('other tests', function(){
+    it('should do the operation', function(){
+        expect(1+1).toEqual(2);
+    })
+
+    it('should match string', function(){
+        expect('hello').toEqual('hello')
+    })
 })
 
-it('should do the operation', function(){
-    expect(1+1).toEqual(2);
+describe('removeDuplicates and remove functions', function(){
+    it('sould remove duplicates from array', function(){
+        expect(removeDuplicates([1,1,2,2,3,3,4,5,6,6])).toEqual([1,2,3,4,5,6])
+        expect(removeDuplicates([1,1,2,2,3,3,4,5,6,6])).toBeInstanceOf(Array)
+    })
+
+
+    it('should remove duplicate from string', function(){
+        expect(removeDuplicates('hello')).toEqual('helo')
+        expect(removeDuplicates('hello')).toBeInstanceOf(String)
+    })
+
+    it('should remove value from array', function(){
+        expect(remove([1,2,3,4,5,6,7,8], 8)).toEqual([1,2,3,4,5,6,7])
+    })
 })
 
-it('should match string', function(){
-    expect('hello').toEqual('hello')
+// special case throwing errors :
+describe('throwing errors', function(){
+    it('should reject invalid incomes', function(){
+        // we have to insert another function before our testing function
+        expect(()=> calculateTaxes('auwdhkawdufawd')).toThrowError();
+        expect(()=> calculateTaxes([])).toThrowError();
+        expect(()=> calculateTaxes(true)).toThrowError();
+    })
+})
+
+describe('submitUser() tests ', ()=>{
+    it('saves input val to usernames array', ()=>{
+        input.value = "something"
+        submitUser();
+        expect(usernames.length).toBe(1);
+        expect(usernames).toContain('something');
+    })
+})
+
+afterEach(()=>{
+    input.value ="";
+    usernames = [];
+    console.log("afterEach!")
 })
